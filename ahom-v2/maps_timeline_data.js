dc171cad97508535453b1a42971e3d32e06fea44
supriyaDutta <34511068+supriyaDutta@users.noreply.github.com>
2019-01-14 00:03:16 +0530

    updated_3
    administration pos corrected

---
+++ ahom-v2/maps_timeline_data.js
@@ -0,0 +1,166 @@
+var map_timeline=[
+  {
+    "map_number": 1,
+    "timeline1": 1228,
+    "timeline2": 1268
+  },
+  {
+    "map_number": 2,
+    "timeline1": 1268,
+    "timeline2": 1497
+  },
+  {
+    "map_number": 3,
+    "timeline1": 1497,
+    "timeline2": 1539
+  },
+  {
+    "map_number": 4,
+    "timeline1": 1539,
+    "timeline2": 1603
+  },
+  {
+    "map_number": 5,
+    "timeline1": 1603,
+    "timeline2": 1648
+  },
+  {
+    "map_number": 6,
+    "timeline1": 1648,
+    "timeline2": 1663
+  },
+  {
+    "map_number": 7,
+    "timeline1": 1663,
+    "timeline2": 1669
+  },
+  {
+    "map_number": 8,
+    "timeline1": 1670,
+    "timeline2": 1677
+  },
+  {
+    "map_number": 9,
+    "timeline1": 1677,
+    "timeline2": 1681
+  },
+  {
+    "map_number": 8,
+    "timeline1": 1681,
+    "timeline2": 1695
+  },
+  {
+    "map_number": 10,
+    "timeline1": 1696,
+    "timeline2": 1806
+  },
+  {
+    "map_number": 11,
+    "timeline1": 1806,
+    "timeline2": 1821
+  },
+  {
+    "map_number": 10,
+    "timeline1": 1821,
+    "timeline2": 1828
+  }
+];
+
+var map_timeline_ad=[
+  {
+    "map_number": 1,
+    "timeline1": 1228,
+    "timeline2": 1268,
+    "maps": "sukapha1"
+  },
+  {
+    "map_number": 2,
+    "timeline1": 1268,
+    "timeline2": 1332,
+    "maps": "suteupha2"
+  },
+  {
+    "map_number": 2,
+    "timeline1": 1332,
+    "timeline2": 1497,
+    "maps": "suteupha2"
+  },
+  {
+    "map_number": 3,
+    "timeline1": 1497,
+    "timeline2": 1539,
+    "maps": "suhungmung3"
+  },
+  {
+    "map_number": 4,
+    "timeline1": 1539,
+    "timeline2": 1552,
+    "maps": "suklengmung4"
+  },
+  {
+    "map_number": 4,
+    "timeline1": 1552,
+    "timeline2": 1603,
+    "maps": "suklengmung4"
+  },
+  {
+    "map_number": 5,
+    "timeline1": 1603,
+    "timeline2": 1648,
+    "maps": "susengpha5"
+  },
+  {
+    "map_number": 6,
+    "timeline1": 1648,
+    "timeline2": 1663,
+    "maps": "sutamla6"
+  },
+  {
+    "map_number": 7,
+    "timeline1": 1663,
+    "timeline2": 1669,
+    "maps": "supungmung7"
+  },
+  {
+    "map_number": 8,
+    "timeline1": 1670,
+    "timeline2": 1677,
+    "maps": "sunyetpha_udayaditya8"
+  },
+  {
+    "map_number": 9,
+    "timeline1": 1677,
+    "timeline2": 1681,
+    "maps": "Sudaipha9"
+  },
+  {
+    "map_number": 8,
+    "timeline1": 1681,
+    "timeline2": 1695,
+    "maps": "sunyetpha_udayaditya8"
+  },
+  {
+    "map_number": 10,
+    "timeline1": 1696,
+    "timeline2": 1751,
+    "maps": "Sukhrangpha_RudraSingh10"
+  },
+  {
+    "map_number": 10,
+    "timeline1": 1751,
+    "timeline2": 1806,
+    "maps": "Sukhrangpha_RudraSingh10"
+  },
+  {
+    "map_number": 11,
+    "timeline1": 1806,
+    "timeline2": 1821,
+    "maps": "sudinpha11"
+  },
+  {
+    "map_number": 10,
+    "timeline1": 1821,
+    "timeline2": 1828,
+    "maps": "Sukhrangpha_RudraSingh10"
+  }
+];
